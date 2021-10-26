2007fba969d7f609b8835050c0b7496775ef722d
Ivan <iwolkkov@gmail.com>
2021-10-26 09:37:09 +0300

    Added d.ts file

--- core/extensions/modules.js
+++ core/extensions/modules.js
@@ -1,34 +1,17 @@
 /*
- * Copyright (c) 2010-2020 SAP and others.
+ * Copyright (c) 2021 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v20.html
  *
- * Contributors:
- *   SAP - initial API and implementation
+ * SPDX-FileCopyrightText: 2021 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ * SPDX-License-Identifier: EPL-2.0
  */
-exports.getContent = function() {
-	return [{
-		name: "core/v4/configurations",
-		description: "Configurations API"
-	}, {
-		name: "core/v4/context",
-		description: "Context API"
-	}, {
-		name: "core/v4/destinations",
-		description: "Destinations API"
-	}, {
-		name: "core/v4/env",
-		description: "Env API"
-	}, {
-		name: "core/v4/exec",
-		description: "Exec API"
-	}, {
-		name: "core/v4/extensions",
-		description: "Extensions API"
-	}, {
-		name: "core/v4/globals",
-		description: "Globals API"
-	}];
+var registry = require('platform/v4/registry');
+exports.getContent = function () {
+    let file = registry.getText("core/extensions/modules.json");
+    return JSON.parse(file);
+
 };
